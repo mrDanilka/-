2fef0418c32481490d16a70e7834d48c0539650b
mrDanilka <danilka.zelen@gmail.com>
2024-09-08 21:30:09 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Number_of_completed_remote_sensing = '12'
+Number_of_hours_spent = '1.5'
+Course_name = 'Python'
+Time_per_task = (float(Number_of_hours_spent)/float(Number_of_completed_remote_sensing))
+print('Курс:',Course_name,', всего задач:',Number_of_completed_remote_sensing,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения',Time_per_task,'часа')
