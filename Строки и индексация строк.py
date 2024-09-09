0f0389b7fe5b5f82b81b3151622e0efcf2059ff6
mrDanilka <danilka.zelen@gmail.com>
2024-09-09 16:19:36 +0300

    Update Строки и индексация строк.py

--- Строки и индексация строк.py
+++ Строки и индексация строк.py
@@ -1,6 +1,6 @@
 example = 'Строки и индексация строк'
 print(example[0])
 print(example[-1])
-print(example [13:])
+print(example [len(example)//2::])
 print(example [::-1])
-print(example [::len(example)%2])
+print(example [1::2])
